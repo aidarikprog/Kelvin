34372de279e7fc4064d2a0551694394f87180fdc
aidarikprog <aidar1171995@mail.ru>
2022-08-31 11:14:06 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const nav = document.querySelector('#nav')
+const navBtn = document.querySelector('#nav-btn')
+const navBtnImg = document.querySelector('#nav-btn-img')
+
+navBtn.onclick =() =>{
+    if (nav.classList.toggle('open')){
+        navBtnImg.src='./img/nav-close.svg'
+    }
+    else{
+        navBtnImg.src='./img/nav-open.svg'
+    }
+}
+
+// or
+
+// navBtn.addEventListener ('click', function (){
+//     if (nav.classList.toggle('open')){
+//         navBtnImg.src='./img/nav-close.svg'
+//     }
+//     else{
+//         navBtnImg.src='./img/nav-open.svg'
+//     }
+// })
